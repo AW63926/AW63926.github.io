586e3abeefe82558520a0a84b4e5abb83f9a12a0
AW63926 <aw63926@gmail.com>
2019-06-23 22:18:11 -0400

    added buttom enlargements

---
+++ app.js
@@ -0,0 +1,22 @@
+var mailImg = document.getElementById('mail');
+mailImg.onmouseover = function() {
+    mailImg.style.width = '15%';
+    mailImg.style.height = '15%';
+
+}
+mailImg.onmouseout = function() {
+    mailImg.style.width = '80px';
+    mailImg.style.width = '80px';
+}
+
+var linkedImg = document.getElementById('linkedIn');
+linkedImg.onmouseover = function() {
+    linkedImg.style.width = '15%';
+    linkedImg.style.height = '15%';
+}
+
+linkedImg.onmouseout = function() {
+    linkedImg.style.height = '8%';
+    linkedImg.style.width = '8%';
+    
+}
